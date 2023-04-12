2df580eb482aca85a7f74f1633b85c666e5182f0
Juliana-placido <juliana.lplacido@icloud.com>
2023-04-12 00:33:50 -0300

    ContaController

--- src/controller/ContaController.java
+++ src/controller/ContaController.java
@@ -1,25 +1,24 @@
 package controller;
-import conta.Conta;
 
 import java.util.ArrayList;
 
+import conta.model.Conta;
+import conta.repository.ContaRepository;
+
 public class ContaController {
-    private ArrayList<Conta> contas;
+    private ContaRepository contaRepository;
 
     public ContaController() {
-        this.contas = new ArrayList<>();
+        this.contaRepository = new ContaRepository();
     }
 
     public void cadastrar(Conta conta) {
-        this.contas.add(conta);
+        this.contaRepository.adicionar(conta);
         System.out.println("Conta cadastrada com sucesso!");
     }
 
     public void atualizar(Conta conta) {
-        int index = procurarIndicePorNumero(conta.getNumero());
-
-        if (index != -1) {
-            this.contas.set(index, conta);
+        if (this.contaRepository.atualizar(conta)) {
             System.out.println("Conta atualizada com sucesso!");
         } else {
             System.out.println("Conta não encontrada!");
@@ -27,10 +26,7 @@ public class ContaController {
     }
 
     public void deletar(int numero) {
-        int index = procurarIndicePorNumero(numero);
-
-        if (index != -1) {
-            this.contas.remove(index);
+        if (this.contaRepository.remover(numero)) {
             System.out.println("Conta deletada com sucesso!");
         } else {
             System.out.println("Conta não encontrada!");
@@ -38,27 +34,54 @@ public class ContaController {
     }
 
     public Conta procurarPorNumero(int numero) {
-        int index = procurarIndicePorNumero(numero);
-
-        if (index != -1) {
-            return this.contas.get(index);
-        } else {
-            return null;
-        }
+        return this.contaRepository.procurarPorNumero(numero);
     }
 
     public ArrayList<Conta> listarTodas() {
-        return this.contas;
+        return this.contaRepository.listarTodas();
     }
 
-    private int procurarIndicePorNumero(int numero) {
-        for (int i = 0; i < this.contas.size(); i++) {
-            Conta conta = this.contas.get(i);
-            if (conta.getNumero() == numero) {
-                return i;
-            }
-        }
+    public void sacar(int numero, double valor) {
+        Conta conta = this.procurarPorNumero(numero);
 
-        return -1;
+        if (conta != null) {
+            if (conta.sacar(valor)) {
+                this.atualizar(conta);
+                System.out.println("Saque realizado com sucesso!");
+            } else {
+                System.out.println("Saldo insuficiente!");
+            }
+        } else {
+            System.out.println("Conta não encontrada!");
+        }
+    }
+
+    public void depositar(int numero, double valor) {
+        Conta conta = this.procurarPorNumero(numero);
+
+        if (conta != null) {
+            conta.depositar(valor);
+            this.atualizar(conta);
+            System.out.println("Depósito realizado com sucesso!");
+        } else {
+            System.out.println("Conta não encontrada!");
+        }
+    }
+
+    public void transferir(int numeroOrigem, int numeroDestino, double valor) {
+        Conta origem = this.procurarPorNumero(numeroOrigem);
+        Conta destino = this.procurarPorNumero(numeroDestino);
+
+        if (origem != null && destino != null) {
+            if (origem.transferir(destino, valor)) {
+                this.atualizar(origem);
+                this.atualizar(destino);
+                System.out.println("Transferência realizada com sucesso!");
+            } else {
+                System.out.println("Saldo insuficiente!");
+            }
+        } else {
+            System.out.println("Conta(s) não encontrada(s)!");
+        }
     }
 }
